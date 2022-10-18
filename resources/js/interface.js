3b54922f11ce5028a6e5d929ecbbcb58df86889f
Laura <la.lo.collett@gmail.com>
2022-10-18 20:29:12 +0100

    making multiple changes and adding js

---
+++ resources/js/interface.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+    $('.header nav .who').hover(function() {
+        $('.content .who').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
+    }, function() {
+        $('.content .who').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
+    });
+    
+    $('.header nav .what').hover(function() {
+        $('.content .what').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
+    }, function() {
+        $('.content .what').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
+    });
+    
+    $('.header nav .how').hover(function() {
+        $('.content .how').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
+    }, function() {
+        $('.content .how').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
+    });
+    
+    $('.header nav .when').hover(function() {
+        $('.content .when').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
+    }, function() {
+        $('.content .when').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
+    });
+    
+    $('.header nav .where').hover(function() {
+        $('.content .where').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
+    }, function() {
+        $('.content .where').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
+    });
+    
+    $('.header nav .why').hover(function() {
+        $('.content .why').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
+    }, function() {
+        $('.content .why').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
+    });
+
+
+    $('.header nav .who').click(function() {
+        $('.content .who').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    });
+
+})
