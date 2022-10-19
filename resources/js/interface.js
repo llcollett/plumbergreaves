4c589ac7ef79b20e211cd3cf942b89992568eaa2
Laura <la.lo.collett@gmail.com>
2022-10-19 10:04:50 +0100

    so many changes

--- resources/js/interface.js
+++ resources/js/interface.js
@@ -1,39 +1,39 @@
 $(document).ready(function(){
 
-    $('.header nav .who').hover(function() {
-        $('.content .who').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
+    $('.header nav #nav-who').hover(function() {
+        $('.content #who').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
     }, function() {
-        $('.content .who').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
+        $('.content #who').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
     });
     
-    $('.header nav .what').hover(function() {
-        $('.content .what').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
+    $('.header nav #nav-what').hover(function() {
+        $('.content #what').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
     }, function() {
-        $('.content .what').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
+        $('.content #what').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
     });
     
-    $('.header nav .how').hover(function() {
-        $('.content .how').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
+    $('.header nav #nav-how').hover(function() {
+        $('.content #how').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
     }, function() {
-        $('.content .how').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
+        $('.content #how').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
     });
     
-    $('.header nav .when').hover(function() {
-        $('.content .when').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
+    $('.header nav #nav-when').hover(function() {
+        $('.content #when').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
     }, function() {
-        $('.content .when').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
+        $('.content #when').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
     });
     
-    $('.header nav .where').hover(function() {
-        $('.content .where').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
+    $('.header nav #nav-where').hover(function() {
+        $('.content #where').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
     }, function() {
-        $('.content .where').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
+        $('.content #where').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
     });
     
-    $('.header nav .why').hover(function() {
-        $('.content .why').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
+    $('.header nav #nav-why').hover(function() {
+        $('.content #why').css({'box-shadow': '0.5rem 0.5rem 1rem black', 'border': '3px solid black'});
     }, function() {
-        $('.content .why').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
+        $('.content #why').css({'box-shadow': '0.125rem 0.125rem 1rem black', 'border': '1px solid black'});
     });
 
 });
